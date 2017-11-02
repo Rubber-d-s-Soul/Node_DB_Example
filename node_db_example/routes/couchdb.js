fbbf48e4b7bd541f574ecbde44a7e6f101e21f72
杉山大輔 <rubber.d.s.soul@gmail.com>
2017-11-02 13:39:42 +0900

    update処理追加

--- node_db_example/routes/couchdb.js
+++ node_db_example/routes/couchdb.js
@@ -22,8 +22,8 @@ const couchAuth = new NodeCouchDb({
     }
 });
 
-
-
+var dbname;
+var dbList;
 /* GET home page. */
 router.get('/', function(req, res, next) {
     var dblist = "";
@@ -43,12 +43,12 @@ router.get('/', function(req, res, next) {
 });
 
 
-router.post('/createdb', function(req, res, next) {
-    var dbList = "";
+router.post('/cruddb', function(req, res, next) {
+
     console.log(req.body.dbselect);
-    var dbname = req.body.dbselect;
+    dbname = req.body.dbselect;
     var dbname_view = dbname.replace("_db", "");
-    var viewUrl = "_design/" + dbname_view + "_view" + "/_view/get_all";
+    var viewUrl = "_design/" + dbname_view + "/_view/get_all";
     console.log(viewUrl);
     couchAuth.get(dbname, viewUrl).then(({ data, headers, status }) => {
         console.log(data);
@@ -58,19 +58,51 @@ router.post('/createdb', function(req, res, next) {
         // headers is an object with all response headers 
         // status is statusCode number 
         var data = {
-            'title': 'CouchDB',
+            'title': 'CouchDB CRUD',
+            'dbname': dbname,
+            'msg': "db success",
             'dbList': dbList
         };
         console.log(data);
-        res.render('couchdb', data);
+        res.render('couchdb_crud', data);
     }, err => {
         console.log(err.body);
         // either request error occured 
         // ...or err.code=EDOCMISSING if document is missing 
         // ...or err.code=EUNKNOWN if statusCode is unexpected 
+        var data = {
+            'title': 'CouchDB CRUD',
+            'dbname': dbname,
+            'msg': "db error",
+            'dbList': null
+        };
+        res.render('couchdb_crud', data);
     });
 })
 
+/*Doc作成*/
+router.post('/createDoc', function(req, res, next) {
+    var titledata = req.body['createDocName'];
+    console.log("[ couch createDoc ]");
+    console.log("[title] : " + titledata);
+
+    couchAuth.insert(dbname, {
+        title: titledata
+    }).then(({ data, headers, status }) => {
+        console.log("[couch createDoc SUCCESS]");
+        console.log("[ status ] : " + status);
+        // location.reload();
+    }, err => {
+        console.log("[couch createDoc ERROR]");
+        console.log(err.body);
+        // location.reload();
+    });
+});
+
+/*Doc 更新*/
+
+
+/*Doc 削除*/
 
 module.exports = router;
 
